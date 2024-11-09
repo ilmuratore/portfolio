c7018af72d54a309d0ddea382b973e93521c1a6b
Simone Iengo <iengo.simone@gmail.com>
2024-11-09 22:50:37 +0100

    fixed some old placeholders in footer and fix an image dont show.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container flex justify-between p-12">
-        <span>LOGO</span>
+        <span>Portfolio.</span>
         <p className="text-slate-600">Simone Iengo. 2024.All rights reserved.</p>
       </div>
     </footer>
