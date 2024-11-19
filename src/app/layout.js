7ebea395d8b263eb712122c16a7660659940e20d
Simone Iengo <iengo.simone@gmail.com>
2024-11-19 10:20:09 +0100

    added more vercel analitics and fixed a bug in deploy phase

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
