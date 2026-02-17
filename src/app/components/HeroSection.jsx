54b8c5636b9d41521d79206f3200a866e91f3cc0
FISSO\iengo <iengo.simone@gmail.com>
2026-02-17 16:02:36 +0100

    update of the CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1LOre7MYU-2TwZKBVVNEIS9PQiwiMWHyH/view?usp=sharing"
+              href="https://drive.google.com/file/d/1LNv14iP7LfDzq6UB6-1hCIXAq3AW67g3/view?usp=sharing"
               className="inline-block w-full px-1 py-1 mt-3 text-white rounded-full sm:w-fit bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
